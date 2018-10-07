c47b6b7511567c7e6488bccf756cd53acd48eb63
Nora <nora.widdecke@tu-bs.de>
2018-10-07 23:47:24 +0200

    add a test

--- src/index.rs
+++ src/index.rs
@@ -1,17 +1,24 @@
 extern crate chrono;
 extern crate libc;
 
-use chrono::{Datelike, Duration};
+use chrono::{Datelike, Duration, NaiveTime};
+use icalwrap::*;
 use std::collections::HashMap;
 use std::fs;
-use ::icalwrap::*;
-use ::utils;
+use utils;
 
 fn get_buckets(comp: &mut Icalcomponent) -> Vec<String> {
   let mut buckets: Vec<String> = comp
     .map(|x| {
       let mut start_date = x.get_dtstart();
-      let end_date = x.get_dtend();
+      let mut end_date = x.get_dtend();
+      //info!("start: {}", start_date);
+      //info!("end: {}", end_date);
+      // end-dtimes are non-inclusive 
+      // so in case of date-only events, the dtend given is one day after the last day of the event
+      if end_date.time() == NaiveTime::from_hms(0, 0, 0) {
+        end_date = end_date.checked_sub_signed(Duration::days(1)).unwrap();
+      }
       let mut buckets = Vec::new();
       while start_date.iso_week() <= end_date.iso_week() {
         let bucket = format!(
@@ -22,6 +29,9 @@ fn get_buckets(comp: &mut Icalcomponent) -> Vec<String> {
         buckets.push(bucket);
         start_date = start_date.checked_add_signed(Duration::days(7)).unwrap();
       }
+      //if buckets.len() > 1 {
+      //  info!("{}: {} buckets", x.get_uid(), buckets.len());
+      //}
       buckets
     }).flatten()
     .collect();
@@ -30,30 +40,36 @@ fn get_buckets(comp: &mut Icalcomponent) -> Vec<String> {
   buckets
 }
 
+fn add_buckets_for_component(buckets: &mut HashMap<String, Vec<String>>, comp: &mut Icalcomponent) {
+  let comp_buckets = get_buckets(comp);
+  for bucketid in comp_buckets {
+    buckets
+      .entry(bucketid)
+      .and_modify(|items| items.push(comp.get_uid()))
+      .or_insert(::utils::vec_from_string(comp.get_uid()));
+  }
+}
+
 pub fn index_dir(dir: &str) {
   let mut buckets: HashMap<String, Vec<String>> = HashMap::new();
 
   if let Ok(entries) = fs::read_dir(dir) {
     for entry in entries {
       if let Ok(entry) = entry {
-        // Here, `entry` is a `DirEntry`.
         if ! entry.path().is_file() {
-          continue
+          continue;
         }
         if entry
           .path()
           .extension()
           .map_or(false, |extension| extension == "ics")
         {
-          if let Ok(contents) = utils::read_file_to_string(&entry.path()) {
-            let mut comp = Icalcomponent::from_str(&contents); //
-            let comp_buckets = get_buckets(&mut comp);
-            for bucketid in comp_buckets {
-              buckets
-                .entry(bucketid)
-                .and_modify(|items| items.push(comp.get_uid()))
-                .or_insert(::utils::vec_from_string(comp.get_uid()));
+          match utils::read_file_to_string(&entry.path()) {
+            Ok(content) => {
+              let mut comp = Icalcomponent::from_str(&content);
+              add_buckets_for_component(&mut buckets, &mut comp);
             }
+            Err(error) => error!("{}", error),
           }
         }
       }
@@ -62,7 +78,26 @@ pub fn index_dir(dir: &str) {
   info!("{} buckets", buckets.len());
   for (key, val) in buckets.iter() {
     if let Err(error) = utils::write_file(key, val.join("\n")) {
-        error!("{}", error);
+      error!("{}", error);
     }
   }
+}
+
+#[test]
+fn buckets_multi_day_allday() {
+  let event_str = "BEGIN:VCALENDAR
+    VERSION:2.0
+    BEGIN:VEVENT
+    UID:20070423T123432Z-541111@example.com
+    DTSTAMP:20070423T123432Z
+    DTSTART;VALUE=DATE:20070628
+    DTEND;VALUE=DATE:20070709
+    SUMMARY:Festival International de Jazz de Montreal
+    TRANSP:TRANSPARENT
+    END:VEVENT
+    END:VCALENDAR";
+
+  let mut comp = Icalcomponent::from_str(event_str);
+  let comp_buckets = get_buckets(&mut comp);
+  assert_eq!(comp_buckets, ["2007-26", "2007-27"]);
 }
