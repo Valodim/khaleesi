e59a8c6b157576cd1d1f5c77630df5ba880316d6
Nora <nora.widdecke@tu-bs.de>
2018-10-31 23:07:36 +0100

    rudimentary formatted agenda output

--- src/agenda.rs
+++ src/agenda.rs
@@ -1,13 +1,16 @@
 use prettyprint::*;
+use utils;
 use std::path::{Path};
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use yansi::Style;
 
 pub fn show_file(filepath: &Path) {
   let f = File::open(filepath).expect("Unable to open file");
   let f = BufReader::new(f);
   let mut lines = f.lines().map(|x| x.expect("Unable to read line"));
-  show_lines(&mut lines);
+  //show_lines(&mut lines);
+  show_events(&mut lines);
 }
 
 fn show_lines(lines: &mut Iterator<Item = String>) {
@@ -16,5 +19,22 @@ fn show_lines(lines: &mut Iterator<Item = String>) {
     let path = Path::new(&line);
     print!("{}  ", i);
     shortprint_file(path)
+  }
+}
+
+fn show_events(lines: &mut Iterator<Item = String>) {
+  let style_heading = Style::new().bold();
+  let comps = utils::read_comps_from_files(lines);
+
+  let mut cur_day = comps[0].get_dtstart().date();
+  println!("{}", style_heading.paint(cur_day));
+
+  for (i, comp) in comps.iter().enumerate() {
+    if (comp.get_dtstart().date() != cur_day) {
+      cur_day = comp.get_dtstart().date();
+      println!("{}", style_heading.paint(cur_day));
+    }
+    print!("  {}  ", i);
+    shortprint_comp(comp)
   }
 }
