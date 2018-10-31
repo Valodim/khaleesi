b1159cfbfafde650b4ea52abad32550698982bb5
Nora <nora.widdecke@tu-bs.de>
2018-10-31 16:56:55 +0100

    correct usage hint

--- src/main.rs
+++ src/main.rs
@@ -38,7 +38,7 @@ fn main() {
     "agenda" => action_agenda(&args[2..]),
     "cal" => cal::printcal(),
     "dbg" => cal::dbg(),
-    _  => println!("Usage: {} index|print|short|cal|dbg", args[0])
+    _  => println!("Usage: {} index|print|agenda|short|cal|dbg", args[0])
   }
 
   // do_other_stuff(args)
