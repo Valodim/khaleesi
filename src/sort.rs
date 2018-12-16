ca8a389b02d73c1f23b905c877935975fc3711a4
Vincent Breitmoser <look@my.amazin.horse>
2018-12-16 21:36:24 +0100

    output index_line for ksort

--- src/sort.rs
+++ src/sort.rs
@@ -4,6 +4,8 @@ pub fn sort_filenames_by_dtstart (files: &mut Iterator<Item = String>) {
   let mut cals = utils::read_calendars_from_files(files).unwrap();
   cals.sort_unstable_by_key(|cal| cal.get_first_event().get_dtstart());
   for cal in cals {
-    println!("{}", cal.get_path_as_string());
+    if let Some(line) = cal.get_first_event().index_line() {
+      println!("{}", line);
+    }
   }
 }
