2adc562782dacbfb9dca35aa499588a5a5ec07b0
Nora <nora.widdecke@tu-bs.de>
2018-12-19 11:29:29 +0100

    refactor list using struct FilterList

--- src/list.rs
+++ src/list.rs
@@ -2,50 +2,94 @@ use chrono::*;
 use icalwrap::IcalVCalendar;
 use utils;
 
-pub fn list_by_args(filenames: &mut Iterator<Item = String>, args: &[String]) {
-  let mut fromarg: Option<Date<Local>> = None;
-  let mut toarg: Option<Date<Local>> = None;
+struct ListFilters {
+  from: Option<Date<Local>>,
+  to: Option<Date<Local>>,
+  num: Option<usize>,
+}
 
-  if args.len() == 1 {
-    if let Ok(num) = args[0].parse::<usize>() {
-      println!("{}", filenames.nth(num).expect("No such element in sequence"));
-      return
+impl ListFilters {
+  pub fn parse_from_args(args: &[String]) -> Result<Self, String> {
+    let mut fromarg: Option<Date<Local>> = None;
+    let mut toarg: Option<Date<Local>> = None;
+
+    if args.len() < 1 {
+      return Err("select [from|to parameter]+".to_string())
     }
-  }
-  if args.len() < 2 {
-    info!("select [from|to parameter]+");
-    return
-  }
 
-  for chunk in args.chunks(2) {
-    if chunk.len() == 2 {
-      let mut datearg = match utils::date_from_str(&chunk[1]) {
-        Ok(datearg) => datearg,
-        Err(error) => {
-          info!("{}", error);
-          return
-        }
-      };
-
-      match chunk[0].as_str() {
-        "from" => fromarg = Some(datearg),
-        "to" => toarg = Some(datearg),
-        _ => { info!("Incorrect!"); return }
+    if args.len() == 1 {
+      if let Ok(num) = args[0].parse::<usize>() {
+        return Ok(ListFilters {from: fromarg, to: toarg, num: Some(num)} );
+      } else { 
+        return Err("select [from|to parameter]+".to_string())
       }
-    } else {
-      info!("Syntax error!");
     }
+
+    for chunk in args.chunks(2) {
+      if chunk.len() == 2 {
+        let mut datearg = match utils::date_from_str(&chunk[1]) {
+          Ok(datearg) => datearg,
+            Err(error) => {
+              return Err(format!("{}", error))
+            }
+        };
+
+        match chunk[0].as_str() {
+          "from" => fromarg = Some(datearg),
+          "to"   => toarg = Some(datearg),
+          _      => return Err("Incorrect!".to_string())
+        }
+      } else {
+        return Err("Syntax error!".to_string());
+      }
+    }
+    Ok(ListFilters {from: fromarg, to: toarg, num: None})
+  }
+
+  pub fn predicate_line_is_from(&self) -> impl Fn(&IcalVCalendar) -> bool + '_ {
+    move |cal| {
+      match &self.from {
+        Some(from) => {
+          let event = cal.get_principal_event();
+          let pred_dtstart = event.get_dtstart().map_or(false, |dtstart| from <= &dtstart.date() );
+          let pred_dtend = event.get_dtend().map_or(false, |dtend| from <= &dtend.date());
+          pred_dtstart || pred_dtend
+        }
+        None => true
+      }
+    }
+  }
+
+  pub fn predicate_line_is_to(&self) -> impl Fn(&IcalVCalendar) -> bool + '_ {
+    move |cal| {
+      match &self.to {
+        Some(to) => {
+          let event = cal.get_principal_event();
+          let pred_dtstart = event.get_dtstart().map_or(true, |dtstart| &dtstart.date() <= to);
+          let pred_dtend = event.get_dtend().map_or(true, |dtend| &dtend.date() <= to);
+          pred_dtstart || pred_dtend
+        }
+        None => true
+      }
+    }
+  }
+}
+
+pub fn list_by_args(filenames: &mut Iterator<Item = String>, args: &[String]) {
+
+  let filters = ListFilters::parse_from_args(args).unwrap();
+
+  if let Some(num) = filters.num {
+    println!("{}", filenames.nth(num).expect("No such element in sequence"));
+    return;
   }
 
   let mut cals = utils::read_calendars_from_files(filenames).unwrap();
 
-  if let Some(fromarg) = fromarg {
-    cals = filter_date_from(cals, fromarg);
-  }
-
-  if let Some(toarg) = toarg {
-    cals = filter_date_to(cals, toarg);
-  }
+  cals = cals.into_iter()
+    .filter( filters.predicate_line_is_from() )
+    .filter( filters.predicate_line_is_to() )
+    .collect();
 
   for cal in cals {
     if let Some(line) = cal.get_principal_event().index_line() {
@@ -54,10 +98,3 @@ pub fn list_by_args(filenames: &mut Iterator<Item = String>, args: &[String]) {
   }
 }
 
-fn filter_date_from(cals: Vec<IcalVCalendar>, from: Date<Local>) -> Vec<IcalVCalendar> {
-  cals.into_iter().filter(|cal| cal.get_principal_event().get_dtstart().unwrap().date() >= from).collect()
-}
-
-fn filter_date_to(cals: Vec<IcalVCalendar>, to: Date<Local>) -> Vec<IcalVCalendar> {
-  cals.into_iter().filter(|cal| cal.get_principal_event().get_dtstart().unwrap().date() <= to).collect()
-}
