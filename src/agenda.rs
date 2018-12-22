b830a628e1ee698a64ee88911885eed2301414f1
Nora <nora.widdecke@tu-bs.de>
2018-12-23 00:08:25 +0100

    print multi-day events on multiple days in agenda

--- src/agenda.rs
+++ src/agenda.rs
@@ -1,45 +1,92 @@
 use utils;
 use icalwrap::*;
-use yansi::{Style,Color};
-use chrono::{NaiveTime, TimeZone, Local};
+use yansi::{Style};
 use config::{self,Config,CalendarConfig};
+use chrono::{TimeZone, Local, Date};
 
 pub fn show_events(config: Config, lines: &mut Iterator<Item = String>) {
-  let style_heading = Style::new().bold();
   let cals = utils::read_calendars_from_files(lines).unwrap();
 
-  let mut cur_day = cals[0].get_principal_event().get_dtstart()
+  let mut not_over_yet: Vec<(usize, &IcalVCalendar)> = Vec::new();
+  let mut cals_iter = cals.iter().enumerate();
+  let mut cur_cal: (usize, &IcalVCalendar);
+
+  match cals_iter.next() {
+    Some(foo) => cur_cal = foo,
+    None => return,
+  }
+  let mut cur_day = cur_cal.1
+    .get_principal_event()
+    .get_dtstart()
     .unwrap_or(Local.timestamp(0, 0))
     .date();
-  println!("{}, {}", style_heading.paint(cur_day.format("%Y-%m-%d")), cur_day.format("%A"));
 
-  for (i, cal) in cals.iter().enumerate() {
-    let cal_config = config::get_config_for_calendar(&config, &cal);
+  let mut events_remaining = true;
+  loop {
+    print_date_line(&cur_day);
 
-    if let Some(start) = cal.get_principal_event().get_dtstart() {
-      if start.date() != cur_day {
-        cur_day = start.date();
-        println!("{}, {}", style_heading.paint(cur_day.format("%Y-%m-%d")), cur_day.format("%A"));
+    for (index, cal) in not_over_yet.clone() {
+      let cal_config = config::get_config_for_calendar(&config, &cal);
+      let event = cal.get_principal_event();
+      print_event_line(cal_config, &index, &event, &cur_day); 
+    }
+    not_over_yet.retain( |(_, cal)| {
+      let event = cal.get_principal_event();
+      event.continues_after(&cur_day) 
+    });
+
+    loop {
+      let cal_config = config::get_config_for_calendar(&config, &cur_cal.1);
+      let event = cur_cal.1.get_principal_event();
+      let i = cur_cal.0;
+
+      match event.get_dtstart() {
+        None => warn!("Invalid DTSTART in {}", event.get_uid()),
+        Some(start) => {
+          if start.date() != cur_day {
+            break;
+          } else {
+            print_event_line(cal_config, &i, &event, &cur_day);
+            if event.continues_after(&cur_day) {
+              not_over_yet.push(cur_cal);
+            }
+
+            match cals_iter.next() {
+              Some(foo) => cur_cal = foo,
+              None => {
+                events_remaining=false;
+                break
+              }
+            }
+          }
+        }
       }
-      match event_line(cal_config, &cal.get_principal_event()) {
-        Ok(line) => println!("{:4}  {}", i, line),
-        Err(error) => warn!("{} in {}", error, cal.get_principal_event().get_uid())
-      }
-    } else {
-      warn!("Invalid DTSTART in {}", cal.get_principal_event().get_uid());
-    };
-
+    }
+    cur_day = cur_day.succ();
+    if not_over_yet.len() == 0 && !events_remaining { break };
   }
 }
 
-pub fn event_line(config: Option<&CalendarConfig>, event: &IcalVEvent) -> Result<String, String> {
+fn print_date_line(date: &Date<Local>) {
+  let style_heading = Style::new().bold();
+  println!("{}, {}", style_heading.paint(date.format("%Y-%m-%d")), date.format("%A"));
+}
+
+fn print_event_line(config: Option<&CalendarConfig>, index: &usize, event: &IcalVEvent, date: &Date<Local>) {
+  match event_line(config, &event, &date) {
+    Ok(line) => println!("{:4}  {}", index, line),
+    Err(error) => warn!("{} in {}", error, event.get_uid())
+  }
+}
+
+pub fn event_line(config: Option<&CalendarConfig>, event: &IcalVEvent, cur_day: &Date<Local>) -> Result<String, String> {
   if event.is_allday() {
     let summary = event.get_summary().ok_or("Invalid SUMMARY")?;
     Ok(format!("             {}", summary))
   } else {
     let mut time_sep = " ";
     let dtstart = event.get_dtstart().ok_or("Invalid DTSTART")?.with_timezone(&Local);
-    let start_string = if dtstart.time() == NaiveTime::from_hms(0, 0, 0) {
+    let start_string = if &dtstart.date() != cur_day {
       "".to_string()
     } else {
       time_sep = "-";
@@ -47,7 +94,7 @@ pub fn event_line(config: Option<&CalendarConfig>, event: &IcalVEvent) -> Result
     };
 
     let dtend = event.get_dtend().ok_or("Invalid DTEND")?.with_timezone(&Local);
-    let end_string = if dtend.time() == NaiveTime::from_hms(0, 0, 0) {
+    let end_string = if &dtend.date() != cur_day {
       "".to_string()
     } else {
       time_sep = "-";
@@ -62,5 +109,14 @@ pub fn event_line(config: Option<&CalendarConfig>, event: &IcalVEvent) -> Result
     }
 
     Ok(format!("{:5}{}{:5}  {}", start_string, time_sep, end_string, summary))
+  }
+}
+
+impl IcalVEvent {
+  fn continues_after(&self, date: &Date<Local>) -> bool {
+    match self.is_allday() {
+      true => self.get_dtend().map( |dtend| dtend.date().pred() > *date),
+      false => self.get_dtend().map( |dtend| dtend.date() > *date)
+    }.unwrap_or(false)
   }
 }
