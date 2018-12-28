2dfc1112e0eb1f913560f630cf3545e88d551a0f
Nora <nora.widdecke@tu-bs.de>
2018-12-28 23:51:25 +0100

    deduplicate file before editing

--- src/edit.rs
+++ src/edit.rs
@@ -3,13 +3,15 @@ use std::process::Command;
 
 pub fn do_edit(filenames: &mut Iterator<Item = String>, _args: &[String]) {
 
-  let paths: Vec<String> = filenames.map( |line| {
+  let mut paths: Vec<String> = filenames.map( |line| {
     let parts: Vec<&str> = line.splitn(2, ' ').collect();
     match utils::datetime_from_timestamp(parts[0]) {
       Some(_) => parts[1].to_string(),
       None => parts[0].to_string(),
     }
   }).collect();
+  paths.sort_unstable();
+  paths.dedup();
 
   Command::new("vim")
     .args(paths)
