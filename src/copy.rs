cfcc25d68fe5aec3efd358c8e1d7ffe6d8865aa4
Nora <nora.widdecke@tu-bs.de>
2019-01-02 00:39:29 +0100

    slightly improve copy

--- src/copy.rs
+++ src/copy.rs
@@ -1,5 +1,4 @@
 use utils;
-use icalwrap::IcalVCalendar;
 
 pub fn do_copy(lines: &mut Iterator<Item = String>, _args: &[String]) {
 
@@ -9,14 +8,13 @@ pub fn do_copy(lines: &mut Iterator<Item = String>, _args: &[String]) {
     return;
   };
 
-  let cal: IcalVCalendar;
-  match utils::read_khaleesi_line(&lines[0]) {
-    Ok(calendar) => cal = calendar,
+  let cal = match utils::read_khaleesi_line(&lines[0]) {
+    Ok(calendar) => calendar,
     Err(error) => {
       error!("{}", error);
       return
     },
-  }
+  };
   let new_cal = match cal.with_uid(&utils::make_new_uid()) {
     Ok(new_cal) => new_cal,
     Err(error) => {
