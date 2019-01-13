386651f16542c4d46bbe45a275db54d570bcc6c6
Nora <nora.widdecke@tu-bs.de>
2019-01-14 00:27:55 +0100

    edit: split off using the editor

--- src/actions/edit.rs
+++ src/actions/edit.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::fs;
+use std::path::Path;
 use std::process::Command;
 
 use backup::backup;
@@ -10,10 +11,14 @@ pub fn do_edit(_args: &[String]) -> Result<(), String> {
   let backup_path = backup(&khline).unwrap();
   info!("Backup written to {}", backup_path.display());
 
+  edit_file(&khline.path)
+}
+
+fn edit_file(path: &Path) -> Result<(), String> {
   let editor = env::var("EDITOR").unwrap_or_else(|_| "vim".to_string());
 
   if let Err(error) = Command::new(&editor)
-    .arg(khline.path.as_os_str())
+    .arg(path.as_os_str())
     .stdin(fs::File::open("/dev/tty").unwrap())
     .status() {
       return Err(format!("{} command failed to start, error: {}", editor, error));
