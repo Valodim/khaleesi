12291396c9297c7f3552d4e2fb2cc4137bf1ccef
Nora <nora.widdecke@tu-bs.de>
2019-01-16 22:19:20 +0100

    clippy: fix useless use of format!

--- src/actions/show.rs
+++ src/actions/show.rs
@@ -7,7 +7,7 @@ pub fn do_show(_args: &[String]) -> Result<(), String> {
   let lines = input::default_input_multiple()?;
 
   for line in lines {
-    let khline = line.parse::<KhLine>().map_err(|err| format!("{}", err))?;
+    let khline = line.parse::<KhLine>().map_err(|err| err.to_string())?;
     let output = fileutil::read_file_to_string(&khline.path).unwrap();
     println!("{}", output);
   }
--- src/icalwrap/icalvcalendar.rs
+++ src/icalwrap/icalvcalendar.rs
@@ -243,7 +243,7 @@ impl IcalVCalendar {
         inner_comp = ical::icalcomponent_get_next_component(comp, ical::icalcomponent_kind_ICAL_ANY_COMPONENT)
       }
 
-      Some(format!("{}", output.join("\n")))
+      Some(output.join("\n").to_string())
     } else {
       IcalVCalendar::check_uid(comp)
     }
