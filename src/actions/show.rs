cdbd28a34a8474ea09a91d2eb93eb89e4b2b4629
Nora <nora.widdecke@tu-bs.de>
2019-01-16 20:31:44 +0100

    show: fix after path change

--- src/actions/show.rs
+++ src/actions/show.rs
@@ -1,19 +1,14 @@
-use std::path::Path;
-
 use input;
-use utils::{fileutil,dateutil};
+use khline::KhLine;
+use utils::fileutil;
 
 pub fn do_show(_args: &[String]) -> Result<(), String> {
   info!("do_show");
   let lines = input::default_input_multiple()?;
 
   for line in lines {
-    let parts: Vec<&str> = line.splitn(2, ' ').collect();
-    let path = match dateutil::datetime_from_timestamp(parts[0]) {
-      Some(_) => Path::new(parts[1]),
-      None => Path::new(parts[0]),
-    };
-    let output = fileutil::read_file_to_string(path).unwrap();
+    let khline = line.parse::<KhLine>().map_err(|err| format!("{}", err))?;
+    let output = fileutil::read_file_to_string(&khline.path).unwrap();
     println!("{}", output);
   }
 
