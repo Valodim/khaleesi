cc0da202d0e3f2c7392cc58896fb18b378369fb8
Nora <nora.widdecke@tu-bs.de>
2019-01-20 00:34:03 +0100

    new: take arguments

--- src/actions/new.rs
+++ src/actions/new.rs
@@ -1,24 +1,65 @@
 use defaults;
 use icalwrap::IcalVCalendar;
 use khline::KhLine;
-use utils::fileutil;
-use utils::misc;
+use utils::{misc,fileutil,dateutil};
+use chrono::{DateTime,Local};
 
 use KhResult;
+use cursorfile;
 
-pub fn do_new(_args: &[&str]) -> KhResult<()> {
+struct EventProperties {
+  calendar: String,
+  start: DateTime<Local>,
+  end: DateTime<Local>,
+  summary: String,
+  location: String
+}
+
+impl EventProperties {
+  fn parse_from_args(args: &[&str]) -> KhResult<EventProperties> {
+    if args.len() < 3 {
+      Err("new calendar from to summary location")?
+    }
+    let calendar = args[0].to_string();
+    let start = dateutil::datetime_from_str(args[1])?;
+    let end = dateutil::datetime_from_str(args[2])?;
+    let summary = args[3].to_string();
+    let location = args[4].to_string();
+    Ok(EventProperties{ calendar, start, end, summary, location })
+  }
+}
+
+pub fn do_new(args: &[&str]) -> KhResult<()> {
   let uid = misc::make_new_uid();
-  let path = defaults::get_datafile(&(uid.clone() + ".ics"));
+  let ep = EventProperties::parse_from_args(args)?;
 
-  let new_cal = IcalVCalendar::from_str(TEMPLATE_EVENT, Some(&path))?.with_uid(&uid)?;
-  let new_cal = new_cal.with_dtstamp_now();
+  let mut path = defaults::get_caldir();
+  path.push(&ep.calendar);
+  path.push(&(uid.clone() + ".ics"));
+
+  let new_cal = IcalVCalendar::from_str(TEMPLATE_EVENT, Some(&path))?
+    .with_uid(&uid)?
+    .with_dtstamp_now()
+    .with_last_modified_now()
+    .with_eventprops(ep);
 
   fileutil::write_cal(&new_cal)?;
 
   let khline = KhLine::from(&new_cal);
+  cursorfile::write_cursorfile(&khline.to_string())?;
   println!("{}", khline);
 
   Ok(())
+}
+
+impl IcalVCalendar {
+  fn with_eventprops(self, ep: EventProperties) -> Self {
+    self
+      .with_dtstart(&ep.start)
+      .with_dtend(&ep.end)
+      .with_summary(&ep.summary)
+      .with_location(&ep.location)
+  }
 }
 
 static TEMPLATE_EVENT: &str = indoc!("
