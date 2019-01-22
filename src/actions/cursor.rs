2b7d90e2600db1b0db115d59707fd57c58dbab6d
Nora <nora.widdecke@tu-bs.de>
2019-01-22 19:30:11 +0100

    cursor: make testable and test

--- src/actions/cursor.rs
+++ src/actions/cursor.rs
@@ -6,7 +6,7 @@ use KhResult;
 
 pub fn do_cursor(_args: &[&str]) -> KhResult<()> {
   if !stdioutils::is_stdin_tty() {
-    write_stdin_to_cursorfile();
+    write_stdin_to_cursorfile()?;
   } else {
     //println!("stdin is tty")
   }
@@ -18,25 +18,63 @@ pub fn do_cursor(_args: &[&str]) -> KhResult<()> {
   Ok(())
 }
 
-fn write_stdin_to_cursorfile() {
-  let lines = match stdioutils::read_lines_from_stdin() {
-    Ok(input) => input,
-    Err(error) => {
-      error!("Error reading from stdin: {}", error);
-      return
-    }
-  };
+fn write_stdin_to_cursorfile() -> KhResult<()> {
+  let lines = stdioutils::read_lines_from_stdin()?;
 
   if lines.len() > 1 {
-    error!("Too many lines on stdin");
-    return
+    Err("Too many lines on stdin")?;
   };
 
-  cursorfile::write_cursorfile(&lines[0]).unwrap();
+  cursorfile::write_cursorfile(&lines[0])?;
+
+  Ok(())
 }
 
 fn write_cursorfile_to_stdout() {
   if let Ok(cursor) = cursorfile::read_cursorfile() {
-    println!("{}", cursor);
+    khprintln!("{}", cursor);
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  use testutils;
+  use assert_fs::prelude::*;
+  use predicates::prelude::*;
+
+  #[test]
+  fn test_with_stdin() {
+    let testdir = testutils::prepare_testdir_empty();
+    let expected_str = "hi there";
+    stdioutils::test_stdin_write(expected_str);
+
+    do_cursor(&[]).unwrap();
+
+    testdir.child(".khaleesi/cursor").assert(expected_str);
+  }
+
+  #[test]
+  fn test_with_stdin_linebreak() {
+    let _testdir = testutils::prepare_testdir_empty();
+    let expected_str = "hi\nthere";
+    stdioutils::test_stdin_write(expected_str);
+
+    let result = do_cursor(&[]);
+
+    assert!(result.is_err());
+    //testdir.child(".khaleesi/cursor").assert(expected_str);
+  }
+
+  #[test]
+  fn test_no_stdin() {
+    let testdir = testutils::prepare_testdir("testdir_with_cursor");
+
+    do_cursor(&[]).unwrap();
+    let out = stdioutils::test_stdout_clear();
+
+    let predicate = predicate::str::similar(out);
+    testdir.child(".khaleesi/cursor").assert(predicate);
   }
 }
