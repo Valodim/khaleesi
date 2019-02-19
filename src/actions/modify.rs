c896b69d52075dbfacb1f439f21eb8b8d4699b8c
Nora <nora.widdecke@tu-bs.de>
2019-02-19 20:44:05 +0100

    khmodify: add --dry-run

--- src/actions/modify.rs
+++ src/actions/modify.rs
@@ -1,19 +1,29 @@
 use crate::input;
 use crate::KhResult;
+use crate::backup::backup;
+use crate::utils::fileutil::write_cal;
 
 pub fn do_modify(args: &[&str]) -> KhResult<()> {
   info!("do_modify");
 
   if args[0] == "removeprop" && args[1] == "xlicerror" {
+    let dry_run = args.len() >= 3 && args[2] == "--dry-run";
+
     let lines = input::default_input_khlines()?;
-    let output: Vec<String> = lines
-      .map(|line| line.to_cal())
-      .flatten()
-      .map(|cal| cal.with_remove_property("X-LIC-ERROR") )
-      .filter(|cal| cal.1 > 0)
-      .map(|cal| cal.0.to_string())
-      .collect();
-    println!("{}", output.join("\n"));
+    for khline in lines {
+      let cal = khline.to_cal()?.with_remove_property("X-LIC-ERROR");
+      if cal.1 > 0 {
+        if !dry_run {
+          println!("Modifying {}", cal.0.get_path_as_string().unwrap());
+
+          let backup_path = backup(&khline).unwrap();
+          info!("Backup written to {}", backup_path.display());
+          write_cal(&cal.0)?
+        } else {
+          println!("Would modify {}", cal.0.get_path_as_string().unwrap());
+        };
+      }
+    };
   } else {
     error!("not supported");
   }
